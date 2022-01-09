17ff8c5f461976e6aec4719045a99a5fb638c8ee
Macho13521 <36113176+Macho13521@users.noreply.github.com>
2022-01-10 00:04:04 +0100

    Dodany Formularz Rejestracji + przechodzenie

--- KomunikatorAI/Rejestracja.cs
+++ KomunikatorAI/Rejestracja.cs
@@ -10,21 +10,16 @@ using System.Windows.Forms;
 
 namespace KomunikatorAI
 {
-    public partial class Logowanie : Form
+    public partial class Rejestracja : Form
     {
-        public Logowanie()
+        public Rejestracja()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            new Rejestracja().Show();
+            new Logowanie().Show();
             this.Hide();
         }
     }
